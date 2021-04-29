1838c87dbe59fecd060739a70b11e34114350549
HealthPDU <health.platform@engineering.digital.dwp.gov.uk>
2021-04-29 13:54:44 +0000

    new release 3.5.0

--- test/unit/lib/validation-rules/date-not-before.spec.js
+++ test/unit/lib/validation-rules/date-not-before.spec.js
@@ -10,66 +10,77 @@ chai.use(chaiAsPromised);
 let dateNotBefore;
 const errorMsg = { inline: 'validation:rule.dateObject.inline', summary: 'validation:rule.dateObject.inline' };
 
+function dateCheck(date) {
+  if (date.mm === 2 && date.dd > 28) {
+    return {
+      dd: 28,
+      mm: date.mm,
+      yyyy: date.yyyy,
+    };
+  }
+  return date;
+}
+
 describe('Date not before validator', () => {
   beforeEach(() => {
     dateNotBefore = require('../../../../app/lib/validation-rules/date-not-before.js'); // eslint-disable-line
   });
   it('should return an error if value is before the default date', () => {
     const dateToCheck = moment().subtract(1, 'day').startOf('day');
-    const value = {
+    const value = dateCheck({
       dd: dateToCheck.date(),
       mm: dateToCheck.month() + 1,
       yyyy: dateToCheck.year(),
-    };
+    });
     return assert.isRejected(dateNotBefore(value), errorMsg);
   });
   it('should return an error if value is before the dateToCheckAgainst provided', () => {
     const dateToCheck = moment().subtract(1, 'day').startOf('day');
-    const value = {
+    const value = dateCheck({
       dd: dateToCheck.date(),
       mm: dateToCheck.month() + 1,
       yyyy: dateToCheck.year(),
-    };
+    });
     const dateToCheckAgainst = () => moment().add(1, 'month');
     dateNotBefore = dateNotBefore.bind({ dateToCheckAgainst });
     return assert.isRejected(dateNotBefore(value), errorMsg);
   });
   it('should not return an error if value is today', () => {
     const dateToCheck = moment().startOf('day');
-    const value = {
+    const value = dateCheck({
       dd: dateToCheck.date(),
       mm: dateToCheck.month() + 1,
       yyyy: dateToCheck.year(),
-    };
+    });
     return assert.isFulfilled(dateNotBefore(value));
   });
   it('should not return an error if value is same as dateToCheckAgainst', () => {
     const dateToCheck = moment().add(1, 'month').startOf('day');
-    const value = {
+    const value = dateCheck({
       dd: dateToCheck.date(),
       mm: dateToCheck.month() + 1,
       yyyy: dateToCheck.year(),
-    };
+    });
     const dateToCheckAgainst = () => moment().add(1, 'month').startOf('day');
     dateNotBefore = dateNotBefore.bind({ dateToCheckAgainst });
     return assert.isFulfilled(dateNotBefore(value));
   });
   it('should not return an error if value is after default date (today) ', () => {
     const dateToCheck = moment().add(1, 'month').startOf('day');
-    const value = {
+    const value = dateCheck({
       dd: dateToCheck.date(),
       mm: dateToCheck.month() + 1,
       yyyy: dateToCheck.year(),
-    };
+    });
     return assert.isFulfilled(dateNotBefore(value));
   });
   it('should not return an error if value is after dateToCheckAgainst', () => {
     const dateToCheck = moment().add(1, 'month').startOf('day');
-    const value = {
+    const value = dateCheck({
       dd: dateToCheck.date(),
       mm: dateToCheck.month() + 1,
       yyyy: dateToCheck.year(),
-    };
+    });
     const dateToCheckAgainst = () => moment().subtract(1, 'month').startOf('day');
     dateNotBefore = dateNotBefore.bind({ dateToCheckAgainst });
     return assert.isFulfilled(dateNotBefore(value));
@@ -87,11 +98,11 @@ describe('Date not before validator', () => {
         hiddenClaimStartDate: claimStartDate,
       },
     };
-    const value = {
+    const value = dateCheck({
       dd: date.date(),
       mm: date.month() + 1,
       yyyy: date.year(),
-    };
+    });
     return assert.isFulfilled(dateNotBefore(value, dataContext));
   });
   it('should return an error if claim-end-date is before claim-start-date', () => {
@@ -107,11 +118,11 @@ describe('Date not before validator', () => {
         hiddenClaimStartDate: claimStartDate,
       },
     };
-    const value = {
+    const value = dateCheck({
       dd: date.date(),
       mm: date.month() - 1,
       yyyy: date.year(),
-    };
+    });
     return assert.isRejected(dateNotBefore(value, dataContext));
   });
   it('should not return an error if claim-start-date is after ssp-end-date', () => {
@@ -127,11 +138,11 @@ describe('Date not before validator', () => {
         hiddenSspEndDate: sspEndDate,
       },
     };
-    const value = {
+    const value = dateCheck({
       dd: date.date(),
       mm: date.month() + 1,
       yyyy: date.year(),
-    };
+    });
     return assert.isFulfilled(dateNotBefore(value, dataContext));
   });
   it('should return an error if claim-start-date is before ssp-end-date', () => {
@@ -147,11 +158,11 @@ describe('Date not before validator', () => {
         hiddenSspEndDate: sspEndDate,
       },
     };
-    const value = {
+    const value = dateCheck({
       dd: date.date(),
       mm: date.month() - 1,
       yyyy: date.year(),
-    };
+    });
     return assert.isRejected(dateNotBefore(value, dataContext));
   });
 });
